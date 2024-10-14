9e546b3a6f0ee82dafb16c676a3f68fe3d9c8bc4
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-10-14 20:41:53 +0530

    basic array problem

---
+++ Array/traversalReverse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void traversalRevers(int arr[], int n){
+  for(int i = n - 1; i <= n - 1; i--){
+  cout << arr[i] << endl;
+  }
+}
+
+int main(){
+    int n;
+     cin >> n;
+    int arr[n];
+    for(int i = 0; i < n - 1; i++){
+        cin >> arr[i];
+    }
+    traversalRevers(arr,n);
+    k
+}
