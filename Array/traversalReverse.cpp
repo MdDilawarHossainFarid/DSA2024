58816c3ef4b805ca3daf6f41e58dbbdb5baf8b4b
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-10-15 20:04:58 +0530

    ReverseArray

--- Array/traversalReverse.cpp
+++ Array/traversalReverse.cpp
@@ -1,19 +1,30 @@
 #include<iostream>
 using namespace std;
 
-void traversalRevers(int arr[], int n){
-  for(int i = n - 1; i <= n - 1; i--){
-  cout << arr[i] << endl;
+void   printReverseArray(int reverserArray[],int n){
+  for(int i = 0; i <= n - 1; i++){
+    cout<<reverserArray[i]<<endl;
   }
+}
+
+
+void traversalRevers(int arr[], int n){
+  int reverserArray[n];
+  for(int i = n - 1 ; i >= 0; i--){
+reverserArray[n - i - 1] = arr[i];
+  }
+  printReverseArray(reverserArray,n);
 }
 
 int main(){
     int n;
      cin >> n;
     int arr[n];
-    for(int i = 0; i < n - 1; i++){
+    for(int i = 0; i <= n - 1; i++){
         cin >> arr[i];
     }
+    cout << endl;
+
     traversalRevers(arr,n);
-    k
+    
 }
